013887df7612c47ec9fff383e89f7fe3dd6a00e9
Brianna <44736324+briannadrew@users.noreply.github.com>
2022-03-15 01:13:42 -0400

    Add files via upload

---
+++ FCFS/FCFS/Cust.cs
@@ -0,0 +1,54 @@
+﻿// Class to hold the customer data
+public class Cust
+{
+    private long arrive_time, int_time;
+    private int cust_num, page_req;
+    private static int num;
+
+    // Constructors
+    public Cust()
+    {
+        arrive_time = 0;
+        int_time = 0;
+        page_req = Utility.Page(Globals.pages);
+        cust_num = num;
+        num += 1;
+    }
+
+    // Return the customer number
+    public int getnum()
+    {
+        return cust_num;
+    }
+
+    // Return the interarrival time
+    public long getiarrive()
+    {
+        return int_time;
+    }
+
+    // Return the arrival time
+    public long getarrive()
+    {
+        return arrive_time;
+    }
+
+    // Return the page requested
+    public int getpage()
+    {
+        return page_req;
+    }
+
+    // Set the arrival time
+    public void setarrive(long time)
+    {
+        arrive_time = time;
+    }
+
+    // Set the interarrival time
+    public void setiarrive(long time)
+    {
+        int_time = time;
+    }
+
+}
